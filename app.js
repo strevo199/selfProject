2cc4b2835a65dec226d214104e7943f11a9645d4
Stephen Mgbeojikwe <mgbeojikwestephen@gmail.com>
2020-02-15 15:42:17 +0100

    my Prosonal Project

---
+++ app.js
@@ -0,0 +1,31 @@
+// const CHaneman  = document.querySelector('.checkbtn')
+// const CHaneman2  = document.querySelector('.checkbtn')
+
+// CHaneman.addEventListener('click',function (e) {
+//     if (e.target.classList.contains('editChange')) {
+//         if (window.matchMedia("(max-width: 690px)").matches) {
+//             CHaneman.innerHTML = '<i class="fa fa-remove backChange" aria-hidden="true"></i>'
+//     document.querySelector('.collection').style.marginTop= '70px'
+//         }
+    
+        
+        
+//     }
+    
+
+//     e.preventDefault()
+// })
+
+// CHaneman2.addEventListener('click',function (e) {
+//     if (e.target.classList.contains('backChange')) {
+//     if (window.matchMedia("(max-width: 690px)").matches) {
+//         CHaneman2.innerHTML = '<i class="fa fa-bars editChange" aria-hidden="true"></i>'
+//         document.querySelector('.collection').style.marginTop= '-200px'
+        
+//     }
+        
+//     }
+    
+
+//     e.preventDefault()
+// })
